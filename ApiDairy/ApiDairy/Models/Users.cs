4364767f6a0467720ef1717e7df35ead605f0fe0
exzotec <evgenulirik2002@bk.ru>
2022-05-19 14:40:17 +0300

    Добавьте файлы проекта.

---
+++ ApiDairy/ApiDairy/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiDairy.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string middle_name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+    }
+}
